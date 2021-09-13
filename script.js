fe23dfaedf35603bbbff6215135038a901fa440d
Iyadi Cyril <iyadicy@gmail.com>
2021-09-13 13:42:11 +0100

    Update script.js
    fix add book function

--- script.js
+++ script.js
@@ -30,30 +30,36 @@ function saveToLocalStorage() {
   localStorage.setItem('books', JSON.stringify(bookArray));
 }
 
-const addButton = document.getElementById('addButton');
-addButton.addEventListener('submit', (e) => {
+function populateTable() {
+  const tableList = document.getElementById('bookList');
+
+  bookArray.forEach((book) => {
+    const tr = document.createElement('tr');
+    const td = document.createElement('td');
+
+    td.innerHTML = `<span>${book.title}</span> by ${book.author}`;
+
+    const removeButton = document.createElement('button');
+    removeButton.id = 'removeButton';
+    removeButton.innerHTML = 'Remove';
+
+    td.appendChild(removeButton);
+    tr.appendChild(td);
+    tableList.appendChild(tr);
+  });
+}
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  
   const titleInput = document.getElementById('book_title');
   const authorInput = document.getElementById('book_author');
   addBook(titleInput.value, authorInput.value);
   saveToLocalStorage();
-});
-
-const tableList = document.getElementById('bookList');
-
-bookArray.forEach((book) => {
-  const tr = document.createElement('tr');
-  const td = document.createElement('td');
-
-  td.innerHTML = `<span>${book.title}</span> by ${book.author}`;
-
-  const removeButton = document.createElement('button');
-  removeButton.id = 'removeButton';
-
-  td.appendChild(removeButton);
-  tr.appendChild(td);
-  tableList.appendChild(tr);
+  populateTable();
+  titleInput.value = '';
+  authorInput.value = '';
 });
 
 const removeButtonArray = Array.from(document.querySelectorAll('#removeButton'));
@@ -62,11 +68,14 @@ removeButtonArray.forEach((removeButton) => {
   removeButton.addEventListener('click', (e) => {
     const clickedTd = e.target;
     console.log(clickedTd);
+    saveToLocalStorage();
+    window.location.reload();
   });
 });
 
 window.onload = () => {
   if (localStorage.getItem('books') !== null) {
     bookArray = JSON.parse(localStorage.getItem('books'));
+    populateTable();
   }
 };
